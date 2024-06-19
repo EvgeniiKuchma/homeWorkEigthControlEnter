f2189dab48d61d7f2fe602c27dc1607fc99c1503
Евгений Кучма <desferto87@gmail.com>
2024-06-19 21:59:44 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkEigthControlEnter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordForExit = "exit";
+            
+            while (true)
+            {
+                Console.Write("Для выхода из программы введите слово " + wordForExit +": ");
+                wordForExit = Console.ReadLine();
+                if (wordForExit == "exit")
+                {
+                    Console.WriteLine("Верное слово");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Попробуйте еще раз");
+                }
+            }
+        }
+    }
+}
